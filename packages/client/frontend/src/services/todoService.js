0914816390e814b64fc4fda8577feafa328c4995
william agre <william.agre@epitech.digital>
2025-12-09 15:32:08 +0100

    feat: removing useless code lines

--- packages/client/frontend/src/services/todoService.js
+++ packages/client/frontend/src/services/todoService.js
@@ -2,7 +2,6 @@ import { API_URL } from "../config";
 
 const STORAGE_KEY = "todos";
 
-/* --- MODE LOCAL (pour l'instant) --- */
 export function getTodos() {
   return JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
 }
@@ -30,20 +29,3 @@ export function deleteTodo(id) {
   const todos = getTodos().filter(todo => todo.id !== id);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
 }
-
-/* --- PRET POUR BACKEND (plus tard) --- */
-/*
-export async function getTodos() {
-  const res = await fetch(`${API_URL}/todos`);
-  return res.json();
-}
-
-export async function addTodo(title) {
-  const res = await fetch(`${API_URL}/todos`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ title })
-  });
-  return res.json();
-}
-*/
